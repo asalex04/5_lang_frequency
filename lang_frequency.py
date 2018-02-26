4257594bf3e0a23400b82c64c0292c35bcd0c4d9
asalex04 <arc04@mail.ru>
2018-02-26 15:53:34 +0400

    created program

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,41 @@
+import sys
+import re
+import collections
 
 
 def load_data(filepath):
-    pass
+   with open(filepath) as text_file:
+       text = text_file.read().lower()
+       return text
 
 
 def get_most_frequent_words(text):
-    pass
+    result = re.findall(r'\w+', text)
+    most_frequent_words = collections.Counter(result).most_common(10)
+    return most_frequent_words
+
+
+def print_words(most_frequent_words):
+    print('Ten most popular words in this text:')
+    counter = 1
+    for i in most_frequent_words:
+        print(str(counter) + '. ' + i[0])
+        counter += 1
 
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) > 1:
+        filepath = format(sys.argv[1])
+    else:
+        filepath = input("Put the path to the file: ")
+    try:
+        text = load_data(filepath)
+    except (FileNotFoundError, ValueError):
+        exit('Not found correct file')
+
+    print_words(get_most_frequent_words(text))
+
+
+
+
